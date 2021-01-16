fe0f5da829542c936828764239974a187612f182
Zolta <rekenei.zolta@gmail.com>
2021-01-16 12:14:47 +0100

    first

---
+++ TesztApp1/TesztApp1/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TesztApp1.Services;
+using TesztApp1.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TesztApp1
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TesztApp1/TesztApp1/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using TesztApp1.ViewModels;
+using TesztApp1.Views;
+using Xamarin.Forms;
+
+namespace TesztApp1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
